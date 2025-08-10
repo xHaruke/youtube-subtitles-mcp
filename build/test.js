eeb204a886c3aadad312247d40808e99f423ebd1
xharuke <amantelcom.aman@gmail.com>
2025-08-10 18:53:54 +0530

    push

--- build/test.js
+++
@@ -1,8 +0,0 @@
-import { fetchSubtitles } from "./scraper/yt-dlp.js";
-async function getSubtitles(videoID, lang) {
-    const subtitle = await fetchSubtitles(videoID, lang);
-    console.log(subtitle);
-    return subtitle;
-    // .map((s) => s.text).join(" ");
-}
-await getSubtitles("uHAx_2bn1kU", "en");
